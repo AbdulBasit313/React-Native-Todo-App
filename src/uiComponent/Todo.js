bac10dc41a2da167a1e7765c0cf77beca896ff43
Abdul Basit <abdul_basit313@outlook.com>
2019-06-07 12:05:58 +0500

    sovled the unhandles promise rejection error when app runs first time

--- src/uiComponent/Todo.js
+++ src/uiComponent/Todo.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, TextInput, TouchableHighlight, TouchableOpacity, FlatList, Keyboard } from 'react-native';
+import { Platform, StyleSheet, Text, View, TextInput, TouchableHighlight, TouchableOpacity, FlatList, Keyboard, ActivityIndicator } from 'react-native';
 import AddButton from './addButton/AddButton';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import ViewContainer from './viewContainer/ViewContainer';
 import { HeaderTitleStyle } from '../routing/stack/methods';
 import AsyncStorage from '@react-native-community/async-storage';
+
 
 class Todo extends Component {
    static navigationOptions = {
@@ -18,10 +19,13 @@ class Todo extends Component {
       this.state = {
          text: '',
          showInput: false,
-         todos: []
+         isLoading: true,
+         todos: [
+            { todo: 'fdsf', id: 1 }
+         ]
       }
       this.handleChange = this.handleChange.bind(this)
-      this.showTodoInput = this.showTodoInput.bind(this)
+      // this.showTodoInput = this.showTodoInput.bind(this)
    }
 
    keyExtractor = (item) => item.id.toString()
@@ -31,24 +35,37 @@ class Todo extends Component {
    }
 
    async componentDidMount() {
+      // let values = JSON.parse(await AsyncStorage.getItem('todos'))
       this.setState({
-         todos: JSON.parse(await AsyncStorage.getItem('todos'))
+         todos: JSON.parse(await AsyncStorage.getItem('todos')),
+         isLoading: false
       })
+      // }
       console.log('componetDidMount', this.state)
    }
 
    // Store data
    storeData = async () => {
-      const todos = [...this.state.todos, { todo: this.state.text, id: Math.random() }]
-      try {
-         await AsyncStorage.setItem('todos', JSON.stringify(todos))
-         this.setState({
-            todos: JSON.parse(await AsyncStorage.getItem('todos')),
-            text: '',
-            showInput: !this.state.showInput
-         })
-      } catch (e) {
-         console.log('error', e)
+      // console.log('runs store data function')
+      // console.log('length', this.state.todos)
+      let notEmpty = this.state.text.trim().length > 0;
+      if (!notEmpty) {
+         alert('Can not submit empty todo')
+      }
+      else {
+         let todos = !this.state.todos ? [{ todo: this.state.text, id: Math.random() }] : [...this.state.todos, { todo: this.state.text, id: Math.random() }]
+         // let todos = [{ todo: this.state.text, id: Math.random() }]
+         // const todos = [...this.state.todos, { todo: this.state.text, id: Math.random() }]
+         try {
+            await AsyncStorage.setItem('todos', JSON.stringify(todos))
+            this.setState({
+               todos: JSON.parse(await AsyncStorage.getItem('todos')),
+               text: '',
+               showInput: !this.state.showInput
+            })
+         } catch (e) {
+            console.log('error', e)
+         }
       }
       console.log('state', this.state)
    }
@@ -71,14 +88,8 @@ class Todo extends Component {
       }
    };
 
-   showTodoInput() {
+   showTodoInput = () => {
       this.setState({ showInput: !this.state.showInput })
-   }
-
-   handleDelete = (id) => {
-      const todos = this.state.todos.filter(item => item.id !== id)
-      console.log('todo', todos)
-      this.setState({ todos })
    }
 
    renderItems = ({ item }) => {
@@ -97,15 +108,35 @@ class Todo extends Component {
    }
 
    render() {
-      const { text, showInput, todos } = this.state
+      const { text, showInput, todos, isLoading } = this.state
       return (
          // <ViewContainer source={require('../assets/images/background-pattern2.jpg')} resizeMode='cover'>
          <ViewContainer>
             <View style={{ flex: 1, position: 'relative' }}>
+               {
+                  isLoading ? (
+                     <View
+                        style={{
+                           ...StyleSheet.absoluteFill,
+                           alignItems: 'center',
+                           justifyContent: 'center'
+                        }}
+                     >
+                        <ActivityIndicator size="large" color="#bad555" />
+                     </View>
+                  ) : null
+               }
                <FlatList
                   data={todos}
                   keyExtractor={this.keyExtractor}
                   renderItem={this.renderItems}
+                  ListEmptyComponent={() => (
+                     <View style={{
+                        flex: 1, alignItems: 'center', justifyContent: 'center', marginTop: 50
+                     }}>
+                        <Text style={{ fontFamily: 'Karla-Regular', fontSize: 18, fontWeight: '400', color: '#F8F8F8' }}>You don't have any todo saved</Text>
+                     </View>
+                  )}
                />
                {showInput ? <View style={{}}>
                   <TextInput
@@ -127,7 +158,7 @@ class Todo extends Component {
                   style={{ position: 'absolute', bottom: 15, right: 15, zIndex: 1 }}
                /> : null}
             </View>
-         </ViewContainer>
+         </ViewContainer >
       );
    }
 }
